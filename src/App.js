306ff483d1b42eaba6119747a40bb968d42708f4
Santiago Zaldivar <santiago.zaldivar.1997@gmail.com>
2021-11-30 15:38:49 -0600

    add number counting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 
+const DATE_TIME = new Date("2021-12-04T16:30:00.000Z").getTime();
+
 function App() {
+  const [seconds, setSeconds] = useState(() => {
+    let timeto = DATE_TIME - new Date().getTime();
+    return (timeto / 1000).toFixed(0);
+  });
+
+  useEffect(() => {
+    let intervalID = setInterval(() => {
+      setSeconds(((DATE_TIME - new Date().getTime()) / 1000).toFixed(0));
+    }, 1000);
+    return () => clearInterval(intervalID);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Test github pages</p>
-      </header>
+      <div className="App-header">
+        <p>{seconds.toLocaleString("es-MX")}</p>
+      </div>
     </div>
   );
 }
