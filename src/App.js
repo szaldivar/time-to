e0d1203145bae2cb02bce141eacd56873ed88f4f
Santiago Zaldivar <santiago.zaldivar.1997@gmail.com>
2021-11-30 22:47:41 -0600

    sorprise on 0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 const DATE_TIME = new Date("2021-12-04T16:30:00.000Z").getTime();
+const DATE_TIME2 = new Date("2021-12-04T18:00:00.000Z").getTime();
 
 function App() {
   const [seconds, setSeconds] = useState(() => {
     let timeto = DATE_TIME - new Date().getTime();
     return Math.floor(timeto / 1000);
   });
+  const [seconds2, setSeconds2] = useState(
+    Math.floor((DATE_TIME2 - new Date().getTime()) / 1000)
+  );
+
+  const interval1Ref = useRef(null);
 
   useEffect(() => {
-    let intervalID = setInterval(() => {
+    interval1Ref.current = setInterval(() => {
       setSeconds(Math.floor((DATE_TIME - new Date().getTime()) / 1000));
     }, 1000);
-    return () => clearInterval(intervalID);
   }, []);
+
+  useEffect(() => {
+    if (seconds < 0) {
+      console.log("here");
+      if (interval1Ref.current) {
+        clearInterval(interval1Ref.current);
+        interval1Ref.current = null;
+        setInterval(() => {
+          setSeconds2(Math.floor((DATE_TIME2 - new Date().getTime()) / 1000));
+        }, 1000);
+      }
+    }
+  }, [seconds]);
 
   return (
     <div className="App">
       <div className="App-header">
-        <p>Faltan {seconds.toLocaleString("es-MX")} segundos para vernos 🙈</p>
+        {seconds > 0 ? (
+          <p>
+            Faltan {seconds.toLocaleString("es-MX")} segundos para vernos 🙈
+          </p>
+        ) : (
+          <div>
+            <img
+              style={{
+                height: 600,
+              }}
+              src="/time-to/ticket.jpg"
+              alt="ticket"
+            />
+            <div>
+              Siguiente sorpresa en {seconds2.toLocaleString("es-MX")} segundos
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
