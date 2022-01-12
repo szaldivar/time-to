6436b0fa263b84d63af8f6ba4e8f44b5ec051fff
Santiago Zaldivar <santiago.zaldivar.1997@gmail.com>
2022-01-12 17:17:06 -0600

    add % progress and bar progress in background

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import { useState, useEffect, useRef } from "react";
 import "./App.css";
 
-const DATE_TIME = new Date("2022-01-22T15:10:00.000Z").getTime();
+const DATE_TIME_LAST_SAW = new Date("2022-01-09T00:45:00.000Z").getTime();
+const DATE_TIME_WILL_SEE = new Date("2022-01-22T15:10:00.000Z").getTime();
 const DATE_TIME_SINCE = new Date("2021-12-12T16:05:00.000Z").getTime();
+const TOTAL_BETWEEN = DATE_TIME_WILL_SEE - DATE_TIME_LAST_SAW;
 
 function App() {
   const [seconds, setSeconds] = useState(() => {
-    let timeto = DATE_TIME - new Date().getTime();
+    let timeto = DATE_TIME_WILL_SEE - new Date().getTime();
     return Math.floor(timeto / 1000);
   });
-  const [seconds2, setSeconds2] = useState(
+  const [seconds2, setSeconds2] = useState(() =>
     Math.floor((new Date().getTime() - DATE_TIME_SINCE) / 1000)
   );
+  const [progress, setProgress] = useState(() => {
+    let timeNow = new Date().getTime();
+    if (timeNow >= DATE_TIME_WILL_SEE) return "100";
+    return (((timeNow - DATE_TIME_LAST_SAW) / TOTAL_BETWEEN) * 100).toFixed(2);
+  });
 
   const interval1Ref = useRef(null);
 
   useEffect(() => {
     interval1Ref.current = setInterval(() => {
-      setSeconds(Math.floor((DATE_TIME - new Date().getTime()) / 1000));
-      setSeconds2(Math.floor((new Date().getTime() - DATE_TIME_SINCE) / 1000));
+      let timeNow = new Date().getTime();
+      setSeconds(Math.floor((DATE_TIME_WILL_SEE - timeNow) / 1000));
+      setSeconds2(Math.floor((timeNow - DATE_TIME_SINCE) / 1000));
+      if (timeNow >= DATE_TIME_WILL_SEE) setProgress("100");
+      else
+        setProgress(
+          (((timeNow - DATE_TIME_LAST_SAW) / TOTAL_BETWEEN) * 100).toFixed(2)
+        );
     }, 1000);
   }, []);
 
@@ -30,8 +43,18 @@ function App() {
             Faltan {seconds.toLocaleString("es-MX")} segundos para vernos 🙈
           </p>
         ) : null}
+        {seconds > 0 ? <p>Llevamos {progress}% de la espera</p> : null}
         <p>Llevamos {seconds2.toLocaleString("es-MX")} segundos juntos ❤️</p>
       </div>
+      <div
+        style={{
+          position: "fixed",
+          inset: 0,
+          transform: `translate(${progress}%)`,
+          backgroundColor: "hsl(220, 13%, 21%)",
+          zIndex: -10,
+        }}
+      ></div>
     </div>
   );
 }
